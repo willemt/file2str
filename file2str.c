4486957e27dcb377dd0bf494c5e1dd11579942c3
Helmut Januschka <helmut@januschka.com>
2015-06-05 22:23:55 +0200

    try to fix coverity discovered issues

--- file2str.c
+++ file2str.c
@@ -10,6 +10,7 @@ char *file2strl(
 {
     FILE *file;
 
+
     if (!(file = fopen(path, "rb")))
     {
         fprintf(stderr, "Unable to open file %s\n", path);
@@ -19,19 +20,23 @@ char *file2strl(
     if (-1 == fseek(file, 0, SEEK_END))
     {
         fprintf(stderr, "Unable to seek file %s\n", path);
+        fclose(file);
         return NULL;
     }
 
     unsigned long file_len;
+    unsigned long bytes_read;
     if (-1 == (file_len = ftell(file)))
     {
         fprintf(stderr, "Unable to ftell() file %s\n", path);
+        fclose(file);
         return NULL;
     }
 
     if (-1 == fseek(file, 0, SEEK_SET))
     {
         fprintf(stderr, "Unable to seek file %s\n", path);
+        fclose(file);
         return NULL;
     }
 
@@ -43,7 +48,12 @@ char *file2strl(
         return NULL;
     }
 
-    fread(contents, file_len, 1, file);
+    bytes_read = fread(contents, file_len, 1, file);
+    if(bytes_read == 0 && ferror(file)) {
+        fprintf(stderr, "Read error");
+        fclose(file);
+        return NULL;   
+    }
     fclose(file);
 
     contents[file_len] = '\0';
