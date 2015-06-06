5deb635fb5630e2b466489d353c18318b396c488
Willem Thiart <himself@willemthiart.com>
2015-06-06 11:48:51 +0900

    Refactored indentation

--- file2str.c
+++ file2str.c
@@ -3,56 +3,57 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *file2strl(
-    const char *path,
-    unsigned int *file_len_out
-)
+char *file2strl(const char *path, unsigned int *file_len_out)
 {
     FILE *file;
+    int e;
 
-    if (!(file = fopen(path, "rb")))
+    file = fopen(path, "rb");
+    if (!file)
     {
         fprintf(stderr, "Unable to open file %s\n", path);
         return NULL;
     }
 
-    if (-1 == fseek(file, 0, SEEK_END))
+    e = fseek(file, 0, SEEK_END);
+    if (-1 == e)
     {
         fprintf(stderr, "Unable to seek file %s\n", path);
         fclose(file);
         return NULL;
     }
 
-    unsigned long file_len;
-    unsigned long bytes_read;
-    if (-1 == (file_len = ftell(file)))
+    long file_len = ftell(file);
+    if (-1 == file_len)
     {
         fprintf(stderr, "Unable to ftell() file %s\n", path);
         fclose(file);
         return NULL;
     }
 
-    if (-1 == fseek(file, 0, SEEK_SET))
+    e = fseek(file, 0, SEEK_SET);
+    if (-1 == e)
     {
         fprintf(stderr, "Unable to seek file %s\n", path);
         fclose(file);
         return NULL;
     }
 
-    char *contents;
-    if (!(contents = malloc(file_len + 1)))
+    char *contents = malloc(file_len + 1);
+    if (!contents)
     {
         fprintf(stderr, "Memory error!\n");
         fclose(file);
         return NULL;
     }
 
-    bytes_read = fread(contents, file_len, 1, file);
-    if(bytes_read == 0 && ferror(file)) {
+    unsigned long bytes_read = fread(contents, file_len, 1, file);
+    if (bytes_read == 0 && ferror(file))
+    {
         fprintf(stderr, "Read error");
         free(contents);
         fclose(file);
-        return NULL;   
+        return NULL;
     }
     fclose(file);
 
@@ -64,10 +65,7 @@ char *file2strl(
     return contents;
 }
 
-char *file2str(
-    const char *path,
-    unsigned long *file_len_out
-)
+char *file2str(const char *path)
 {
-    return file2strl(path,NULL);
+    return file2strl(path, NULL);
 }
